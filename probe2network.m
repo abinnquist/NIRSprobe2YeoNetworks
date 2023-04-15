fd7645fc27ec17ae74bb1017e65ab766c26977fe
abinnquist <57020313+abinnquist@users.noreply.github.com>
2023-04-14 18:10:31 -0700

    Create probe2network.m

---
+++ probe2network.m
@@ -0,0 +1,17 @@
+%% INPUTS
+%Below input will be specific to you storage location
+preprocess_dir = 'C:\Users\Mike\Desktop\Yeo_Networks\example'; %location of channel_mnicoords.csv
+yeodir = 'C:\Users\Mike\Desktop\Yeo_Networks'; %location of Yeo network info
+nparc = 42; %Number of channels
+YeoNum = 7; %7 or 17 networks
+
+%% Function to get Yeo Networks based on probe MNI coordinates
+addpath(genpath(yeodir)) %Make sure this path is where ever you have the scripts located
+
+yeoNetNames = getyeonetnames(preprocess_dir, yeodir, nparc, YeoNum);
+
+% To add a channel number column (not really necessary)
+channels=1:nparc;
+yeoNetNames.Channel =channels';
+
+
